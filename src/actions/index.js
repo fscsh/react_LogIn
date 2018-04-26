bda8e6b2a4cab6c2b29cd64497a6ae95ae126b15
Shuhao Chen <shuhao.chen@sjsu.edu>
2018-04-26 13:08:43 -0700

    gethomeroute

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {browserHistory} from 'react-router';
-import {AUTH_USER, UNAUTH_USER, AUTH_ERROR} from './type'
+import {AUTH_USER, UNAUTH_USER, AUTH_ERROR, FETCH_MESSAGE} from './type'
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -22,16 +22,15 @@ export function signInUser({email, password}) {
 
 export function signupUser({ email, password }) {
   return function(dispatch) {
-      console.log({ email, password });
+      // console.log({ email, password });
     axios.post(`${ROOT_URL}/signup`, { email, password })
-
-    .then(response => {
-      dispatch({ type: AUTH_USER });
-      localStorage.setItem('token', response.data.token);
-      browserHistory.push('/feature');
-    })
-    .catch(({response}) => dispatch(authError(response.data.error)));
-  }
+        .then(response => {
+            dispatch({ type: AUTH_USER });
+            localStorage.setItem('token', response.data.token);
+            browserHistory.push('/feature');
+        })
+        .catch(({response}) => dispatch(authError(response.data.error)));
+        }
 }
 
 export function authError(error) {
@@ -41,4 +40,18 @@ export function authError(error) {
 export function signoutUser() {
     localStorage.removeItem('token');
     return {type: UNAUTH_USER}
+}
+
+export function fetchMessage(){
+    return function(dispatch){
+        axios.get(ROOT_URL,{
+            headers: { authorization: localStorage.getItem('token')}
+        })
+            .then(response=>{
+                dispatch({
+                    type: FETCH_MESSAGE,
+                    payload:response.data.message
+                })
+            })
+    }
 }
